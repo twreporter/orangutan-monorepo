d256c42f9b116a88a24068fb0f0fe33f220758fa
nickhsine <nickhsine97753017@gmail.com>
2020-03-25 10:50:49 +0800

    chore(dual-channel): export getWebpackEntry function

--- packages/dual-channel/src/index.js
+++ packages/dual-channel/src/index.js
@@ -1,9 +1,13 @@
 import ReactComponent from './app'
 import Sheets from './sheets'
+import path from 'path'
 import { buildEmbeddedCode } from './build-code'
 
 export default {
   ReactComponent,
   Sheets,
   buildEmbeddedCode,
+  getWebpackEntry: () => {
+    return path.resolve(__dirname, './build-code/client.js')
+  },
 }
