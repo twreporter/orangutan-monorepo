58e147783b6d0f1f060bc47252719ee1873906dc
Taylor Fang <stu005606@gmail.com>
2020-04-07 20:07:34 +0800

    refactor(scrollable-image-ui): use orangutan to build code

--- packages/scrollable-image-ui/webpack.config.js
+++ packages/scrollable-image-ui/webpack.config.js
@@ -6,6 +6,7 @@ const devDirname = 'dev'
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 const config = {
+  target: 'node',
   mode: isDevelopment ? 'development' : 'production',
   entry: './src/client.js',
   output: {
