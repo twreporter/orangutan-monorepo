ec11e44f58cba359da8658d0596f72bc3bd1b651
Taylor Fang <stu005606@gmail.com>
2020-09-14 18:48:51 +0800

    feat(zoom-in-ui): appearance setting

--- packages/zoom-in-ui/src/components/app.js
+++ packages/zoom-in-ui/src/components/app.js
@@ -1,26 +1,29 @@
-import AddImageLinkForm from './form'
+import AddImageForm from './form'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
+import Setting from './setting'
 import themes from '../themes'
 import useImagesState from '../hooks/use-images-state'
 import zoomIn from '@twreporter/zoom-in'
 // lodash
-import assign from 'lodash/assign'
 import get from 'lodash/get'
 import map from 'lodash/map'
+import merge from 'lodash/merge'
 // @material-ui
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
 import Container from '@material-ui/core/Container'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
+import RedoIcon from '@material-ui/icons/Redo'
 import Typography from '@material-ui/core/Typography'
+import DevicesIcon from '@material-ui/icons/Devices'
 import { makeStyles } from '@material-ui/core/styles'
 
 const _ = {
-  assign,
   get,
   map,
+  merge,
 }
 
 const useStyles = makeStyles(theme => ({
@@ -37,27 +40,27 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const getImagesFromSearch = () => {
-  if (typeof window !== 'undefined') {
-    const searchParams = new URLSearchParams(window.location.search)
-    return searchParams.getAll('image')
-  }
-}
-
 const App = props => {
   const { description, title } = props
   const classes = useStyles()
-  const { imageLinks, addImageLink } = useImagesState(getImagesFromSearch())
+  const { imageLinks, addImageLink } = useImagesState([])
+  const [imageCaption, addImageCaption] = useState()
+  const [theme, setTheme] = useState(_.merge({}, themes.twreporterTheme))
 
-  const handleSaveLink = link => {
-    const trimmedLink = link.trim()
+  const handleSaveLink = ({ imgUrl, caption }) => {
+    const trimmedLink = imgUrl.trim()
     if (trimmedLink.length > 0) {
       addImageLink(trimmedLink)
+      addImageCaption(caption)
     }
   }
 
-  const ZoomableImage = zoomIn.Component
+  const updateTheme = updatedPart => {
+    const updatedTheme = _.merge({}, theme, updatedPart)
+    setTheme(updatedTheme)
+  }
 
+  const ZoomableImage = zoomIn.Component
   return (
     <>
       <CssBaseline />
@@ -72,19 +75,54 @@ const App = props => {
             ))}
           </Typography>
           <Divider className={classes.divider} variant="middle" />
-          <AddImageLinkForm
+          <AddImageForm
             buttonLabel="Submit"
-            placeholder="public URL of your image"
+            placeholder={[
+              'public URL of your image',
+              'caption for the image (optional)',
+            ]}
             submitHandler={handleSaveLink}
             icon={<ArrowDownwardIcon />}
           />
           {imageLinks && imageLinks.length > 0 ? (
             <div className={classes.sandbox}>
+              <Setting
+                theme={theme}
+                updateTheme={updateTheme}
+                caption={imageCaption}
+              />
               <ZoomableImage
                 src={imageLinks[imageLinks.length - 1]}
-                caption="數年前在美國使用VRS手語視訊翻譯平台（Video Relay Service）後，難忘如此振奮的感覺，歐陽磊回台便創立洛以，全體員工都是聽障者。（攝影／張家瑋）"
-                theme={themes.twreporterTheme}
+                caption={imageCaption}
+                theme={theme}
               />
+              <Typography
+                style={{ textAlign: 'right', color: 'grey' }}
+                variant="body1"
+                component="div"
+                gutterBottom
+              >
+                <span>
+                  <RedoIcon style={{ transform: 'rotate(243deg)' }} />
+                </span>
+                click to preview
+              </Typography>
+              <Typography
+                style={{ fontSize: '14px', color: 'grey', marginTop: '20px' }}
+                variant="body1"
+                component="div"
+                gutterBottom
+              >
+                <span>
+                  <DevicesIcon
+                    style={{ transform: 'translateY(25%)', marginRight: '5px' }}
+                  />
+                </span>
+                <span>
+                  Resize the browser window to emulate various screen
+                  resolutions.
+                </span>
+              </Typography>
             </div>
           ) : null}
         </Paper>
