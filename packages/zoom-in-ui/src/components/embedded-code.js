60b1badd59c7429481245a4cd678f7ea90017728
Taylor Fang <stu005606@gmail.com>
2020-09-15 12:20:40 +0800

    feat(zoom-in-ui): build embedded code

--- packages/zoom-in-ui/src/components/embedded-code.js
+++ packages/zoom-in-ui/src/components/embedded-code.js
@@ -6,33 +6,29 @@ import Alert from '@material-ui/lab/Alert'
 import AlertTitle from '@material-ui/lab/AlertTitle'
 import Button from '@material-ui/core/Button'
 import CopyIcon from '@material-ui/icons/FileCopy'
-import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
-
-// TODO: move this link into `cloud-function`
-const questionnaireLink = 'https://forms.gle/TidE4vNBzsPWECXDA'
 
 const useStyles = makeStyles(theme => {
   return {
     result: {
       marginTop: '50px',
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
+      backgroundColor: theme.palette.background.default,
+      padding: '20px',
     },
     error: {
       minWidth: 400,
-      boxShadow: theme.shadows[5],
     },
     codeBlock: {
-      padding: theme.spacing(1),
-      margin: theme.spacing(2),
+      padding: `${theme.spacing(1)}px 0`,
     },
     copyBtn: {
       marginTop: theme.spacing(1),
       marginBottom: theme.spacing(2),
+    },
+    alignCenter: {
+      textAlign: 'center',
     },
   }
 })
@@ -70,7 +66,7 @@ const EmbeddedCodeModal = ({ header, description, code, buildCodeError }) => {
           {header}
         </Typography>
         <Typography variant="body1">{description}</Typography>
-        <Paper className={classes.codeBlock} elevation={3}>
+        <div className={classes.codeBlock}>
           <TextField
             fullWidth
             multiline
@@ -79,30 +75,29 @@ const EmbeddedCodeModal = ({ header, description, code, buildCodeError }) => {
             variant="filled"
             InputProps={{ disableUnderline: true }}
           />
-        </Paper>
-        <Button
-          className={classes.copyBtn}
-          disabled={!code}
-          endIcon={<CopyIcon />}
-          variant="contained"
-          onClick={() => {
-            copy(code)
-              .then(() => {
-                setIsCopied(true)
-                setTimeout(() => {
-                  setIsCopied(false)
-                }, 700)
-              })
-              .catch(error => {
-                console.error(error)
-              })
-          }}
-        >
-          {isCopied ? 'COPIED ' : 'CLICK TO COPY'}
-        </Button>
-        <a href={questionnaireLink} target="_blank" rel="noopener noreferrer">
-          <Typography variant="body1">Please give us your feedback</Typography>
-        </a>
+        </div>
+        <div className={classes.alignCenter}>
+          <Button
+            className={classes.copyBtn}
+            disabled={!code}
+            endIcon={<CopyIcon />}
+            variant="contained"
+            onClick={() => {
+              copy(code)
+                .then(() => {
+                  setIsCopied(true)
+                  setTimeout(() => {
+                    setIsCopied(false)
+                  }, 700)
+                })
+                .catch(error => {
+                  console.error(error)
+                })
+            }}
+          >
+            {isCopied ? 'COPIED ' : 'CLICK TO COPY'}
+          </Button>
+        </div>
       </div>
     )
   }
