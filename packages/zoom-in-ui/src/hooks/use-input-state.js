85776c32c1d7ae6e2ed6037f312c64059ae6cb3c
Taylor Fang <stu005606@gmail.com>
2020-09-16 10:47:58 +0800

    feat(zoom-in-ui): provide image url and caption via search params

--- packages/zoom-in-ui/src/hooks/use-input-state.js
+++ packages/zoom-in-ui/src/hooks/use-input-state.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-export default () => {
-  const [value, setValue] = useState('')
+export default initialValue => {
+  const [value, setValue] = useState(initialValue)
 
   return {
     value,
