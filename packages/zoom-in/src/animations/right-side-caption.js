b72fa102dcc24e1ca398e7681b8d1e89d0817145
Taylor Fang <stu005606@gmail.com>
2020-10-12 16:56:35 +0800

    refactor(zoom-in): pass node instead of ref into animations

--- packages/zoom-in/src/animations/right-side-caption.js
+++ packages/zoom-in/src/animations/right-side-caption.js
@@ -3,27 +3,28 @@ import { isSvg } from '../utils'
 /**
  * animate function for zoom component
  * @param {Object} config - The config object for animation
- * @param {HTMLDivElement | null} config.originalRef
- * @param {HTMLDivElement | null} config.zoomedRef
+ * @param {HTMLDivElement | null} config.originalNode
+ * @param {HTMLDivElement | null} config.zoomedNode
+ * @param {HTMLDivElement | null} config.captionNode
  * @param {import('../typedef').Theme} config.themeContext
  * @param {number} config.captionHeight
  * @param {number} clientWidth
  * @param {number} clientHeight
  */
 const animate = ({
-  originalRef,
-  zoomedRef,
-  captionRef,
+  originalNode,
+  zoomedNode,
+  captionNode,
   themeContext,
   captionHeight,
   clientWidth,
   clientHeight,
 }) => {
-  if (!originalRef.current || !zoomedRef.current) return
+  if (!originalNode || !zoomedNode) return
   const { frame, image, caption, zoomOptions } = themeContext
   const { transitionDuration, transitionFunction } = zoomOptions
 
-  const zoomTarget = originalRef.current
+  const zoomTarget = originalNode
 
   const captionWidth = caption.width + caption.marginLeft + caption.marginRight
 
@@ -52,8 +53,8 @@ const animate = ({
 
   const transform = `scale(${scale}) translate3d(${targetTranslateX}px, ${targetTranslateY}px, 0)`
   const transition = `transform ${transitionDuration}ms ${transitionFunction}`
-  zoomedRef.current.style.transition = transition
-  zoomedRef.current.style.transform = transform
+  zoomedNode.style.transition = transition
+  zoomedNode.style.transform = transform
 
   const captionTranslateX =
     targetTranslateX * scale + (width + scale * width) / 2 + image.marginRight
@@ -67,8 +68,8 @@ const animate = ({
         (height + scale * height) / 2 -
         (caption.marginBottom + captionHeight)
 
-  if (captionRef.current) {
-    captionRef.current.setAttribute(
+  if (captionNode) {
+    captionNode.setAttribute(
       'style',
       `position: absolute;
       top: 0;
