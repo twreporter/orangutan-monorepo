4e2a776189bf7d2c3bf46d07c9c9667646320d8c
Taylor Fang <stu005606@gmail.com>
2020-11-03 16:32:41 +0800

    feat(orangutan): add `zoom-in`

--- packages/orangutan/webpack-entry.js
+++ packages/orangutan/webpack-entry.js
@@ -1,7 +1,9 @@
 const dualChannel = require('@twreporter/dual-channel').default
 const scrollableImage = require('@twreporter/scrollable-image').default
+const zoomIn = require('@twreporter/zoom-in').default
 
 module.exports = {
   'dual-channel': dualChannel.getWebpackEntry(),
   'scrollable-image': scrollableImage.getWebpackEntry(),
+  'zoom-in': zoomIn.getWebpackEntry(),
 }
