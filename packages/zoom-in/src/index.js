bcfd2577fdf619dffa990afa7f843d87026698bb
Taylor Fang <stu005606@gmail.com>
2020-11-06 19:06:29 +0800

    refactor: remove `getWebpackEntry` and `getTestData` from export object
    This patch removes `getWebpackEntry` and `getTestData` from export
    object since the `orangutan` repo is removed.

--- packages/zoom-in/src/index.js
+++ packages/zoom-in/src/index.js
@@ -1,13 +1,7 @@
 import Component from './components/zoomable-image'
-import path from 'path'
 import { buildEmbeddedCode } from './build-code'
-import { mockData } from './test-data/data'
 
 export default {
   Component,
   buildEmbeddedCode,
-  getWebpackEntry: () => {
-    return path.resolve(__dirname, './build-code/client.js')
-  },
-  getTestData: () => mockData,
 }
