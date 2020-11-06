ece6111a47bca0b511099a004b06b2938fcce005
Taylor Fang <stu005606@gmail.com>
2020-11-06 17:55:54 +0800

    chore(orangutan): remove orangutan repo
    This patch removes orangutan repo which used to be an all-in-one npm package.
    
    BREAKING CHANGE: remove orangutan repo

--- packages/orangutan/webpack-entry.js
+++
@@ -1,9 +0,0 @@
-const dualChannel = require('@twreporter/dual-channel').default
-const scrollableImage = require('@twreporter/scrollable-image').default
-const zoomIn = require('@twreporter/zoom-in').default
-
-module.exports = {
-  'dual-channel': dualChannel.getWebpackEntry(),
-  'scrollable-image': scrollableImage.getWebpackEntry(),
-  'zoom-in': zoomIn.getWebpackEntry(),
-}
