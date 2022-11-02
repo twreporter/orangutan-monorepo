7ea558bdfb10eae6d5bc20c94a2f8ae2aaf48174
duidae <duidae@gmail.com>
2022-11-02 09:14:01 +0800

    fix: add check for SSR

--- packages/dual-channel/src/app/constants/customized-props.js
+++ packages/dual-channel/src/app/constants/customized-props.js
@@ -1,4 +1,16 @@
-export const scrollableAncestor =
-  document.currentScript.getAttribute('data-scrollable-ancestor') || undefined
+const isClientSide =
+  typeof window !== 'undefined' && typeof window.document !== 'undefined'
+
+let scrollableAncestorValue =
+  isClientSide &&
+  document.currentScript.getAttribute('data-scrollable-ancestor')
+export const scrollableAncestor = scrollableAncestorValue || undefined
+
+let offsetTopValue =
+  isClientSide && document.currentScript.getAttribute('data-offset-top')
 export const offsetTop =
-  document.currentScript.getAttribute('data-offset-top') || 0
+  offsetTopValue &&
+  Number.isInteger(Number(offsetTopValue)) &&
+  Number(offsetTopValue) > 0
+    ? offsetTopValue
+    : 0
